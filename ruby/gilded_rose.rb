d02a481770e87815ae1517e42b23bc323e25083e
Justinio14 <jbutwell@hotmail.com>
2017-03-08 12:17:55 +0000

    item_selection method refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -2,17 +2,16 @@ class GildedRose
 
   attr_reader :name, :sell_in, :quality
 
+  QUALITY_FLOOR = 0
+  QUALITY_CAP = 50
+  SULFURAS_QUALITY = 80
+
   def initialize(items)
     @items = items
-    @specials = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", "Conjured" ]
     @name = ""
     @quality = 0
     @sell_in = 0
   end
-
-  QUALITY_FLOOR = 0
-  QUALITY_CAP = 50
-  SULFURAS_QUALITY = 80
 
   def item_display()
     @items.each do |item|
@@ -23,75 +22,77 @@ class GildedRose
     quality_limit
   end
 
+  def update_quality()
+    item_display
+    sell_in_reduce
+    item_selection
+  end
+
+  private
+
   def quality_limit
     @quality > QUALITY_CAP ? @quality = QUALITY_CAP : @quality = @quality
   end
 
+  def sell_in_reduce
+    @name == "Sulfuras, Hand of Ragnaros" ? @sell_in : @sell_in -= 1
+  end
 
-
-  def update_quality()
-    item_display
-    sell_in_reduce
-    if @specials[0] == @name
+  def item_selection
+    if @name == "Aged Brie"
       aged_brie
-    elsif @specials[1] == @name
+    elsif @name == "Backstage passes to a TAFKAL80ETC concert"
       backstage
-    elsif @specials[2] == @name
+    elsif @name == "Sulfuras, Hand of Ragnaros"
       sulfuras
-    elsif @specials[3] == @name
+    elsif @name == "Conjured"
       conjured
     else
       standard
     end
   end
 
-  private
+  def aged_brie
+    @quality < QUALITY_CAP  ? @quality += 1 : @quality = QUALITY_CAP
+  end
 
-    def aged_brie
-      @quality < QUALITY_CAP  ? @quality += 1 : @quality = QUALITY_CAP
-    end
+  def sulfuras
+    @quality = SULFURAS_QUALITY
+  end
 
-    def sulfuras
-      @quality = SULFURAS_QUALITY
+  def backstage
+    if @sell_in < 0
+      @quality = QUALITY_FLOOR
+    elsif @sell_in < 6 && @quality <= (QUALITY_CAP - 3)
+      @quality += 3
+    elsif @sell_in < 11 && @quality <= (QUALITY_CAP - 2)
+      @quality += 2
+    elsif @sell_in > 10 && @quality <= (QUALITY_CAP - 1)
+      @quality += 1
+    else
+      @quality = QUALITY_CAP
     end
+  end
 
-    def backstage
-      if @sell_in < 0
-        @quality = QUALITY_FLOOR
-      elsif @sell_in < 6 && @quality <= (QUALITY_CAP - 3)
-        @quality += 3
-      elsif @sell_in < 11 && @quality <= (QUALITY_CAP - 2)
-        @quality += 2
-      elsif @sell_in > 10 && @quality <= (QUALITY_CAP - 1)
-        @quality += 1
-      else
-        @quality = QUALITY_CAP
-      end
-    end
+  def conjured
+    if @sell_in >= 0 && @quality > (QUALITY_FLOOR + 1)
+      @quality -= 2
+    elsif @sell_in < 0 && @quality > (QUALITY_FLOOR + 3)
+      @quality -= 4
+    else
+      @quality = QUALITY_FLOOR
+   end
+  end
 
-    def conjured
-      if @sell_in >= 0 && @quality > (QUALITY_FLOOR + 1)
-        @quality -= 2
-      elsif @sell_in < 0 && @quality > (QUALITY_FLOOR + 3)
-        @quality -= 4
-      else
-        @quality = QUALITY_FLOOR
-     end
+  def standard
+    if @sell_in < 0 &&  @quality > (QUALITY_FLOOR + 2)
+      @quality -= 2
+    elsif @sell_in >= 0 &&  @quality > (QUALITY_FLOOR + 1)
+      @quality -= 1
+    else
+      @quality = QUALITY_FLOOR
     end
-
-    def standard
-      if @sell_in < 0 &&  @quality > (QUALITY_FLOOR + 2)
-        @quality -= 2
-      elsif @sell_in >= 0 &&  @quality > (QUALITY_FLOOR + 1)
-        @quality -= 1
-      else
-        @quality = QUALITY_FLOOR
-      end
-    end
-
-    def sell_in_reduce
-      @name == @specials[2] ? @sell_in : @sell_in -= 1
-    end
+  end
 
 end
 
